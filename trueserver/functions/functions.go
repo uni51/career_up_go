91230e9c5755be6216cfd89fd24d021b8f511fcf
uni51 <unieye51@gmail.com>
2024-02-05 13:44:19 +0900

    3-2 Goの関数

--- trueserver/functions/functions.go
+++ trueserver/functions/functions.go
@@ -3,3 +3,21 @@ package functions
 func Add(a int, b int) int {
 	return a + b
 }
+
+func Sub(a, b int) (string, int) {
+	return "%d-%d は %d なのだ", a - b
+}
+
+func AddAll(sl []int, a int) {
+	for i := 0; i < len(sl); i++ {
+		sl[i] += a
+	}
+}
+
+func AddAndCopy(sl []int, a int) []int {
+	sl_cp := []int{}
+	for i := 0; i < len(sl); i++ {
+		sl_cp = append(sl_cp, sl[i]+a)
+	}
+	return sl_cp
+}
--- trueserver/trueserver.go
+++ trueserver/trueserver.go
@@ -14,8 +14,46 @@ func add(writer http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(writer, "5+7=%d", result)
 }
 
+func sub(writer http.ResponseWriter, req *http.Request) {
+	a, b := 5, 7
+
+	output, result := functions.Sub(a, b)
+
+	fmt.Fprintln(writer, "*** 複数の値を戻す関数 ***")
+	fmt.Fprintf(writer, output, a, b, result)
+}
+
+func with_slices(writer http.ResponseWriter, req *http.Request) {
+	sl_1 := []int{1, 2, 3, 4}
+
+	fmt.Fprintln(writer, "*** スライスそのものを書き換える ***")
+
+	// 処理前の値を確かめる
+	fmt.Fprintln(writer, "\nsl_1は")
+	fmt.Fprintln(writer, sl_1)
+
+	functions.AddAll(sl_1, 9)
+
+	fmt.Fprintln(writer, "\nいまやsl_1は")
+	fmt.Fprintln(writer, sl_1)
+
+	fmt.Fprintln(writer, "\n*** スライスをコピーして書き換える ***")
+
+	sl_2 := functions.AddAndCopy(sl_1, 100)
+
+	// 新しくできたsl_2
+	fmt.Fprintln(writer, "\nsl_2は")
+	fmt.Fprintln(writer, sl_2)
+
+	// 元のsl_1
+	fmt.Fprintln(writer, "\n一方sl_1は")
+	fmt.Fprintln(writer, sl_1)
+}
+
 func main() {
 	http.HandleFunc("/add", add)
+	http.HandleFunc("/sub", sub)
+	http.HandleFunc("/with_slices", with_slices)
 
 	http.ListenAndServe(":8090", nil)
 }
